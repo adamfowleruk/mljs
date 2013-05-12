c10c370fb5c6e8cebdbda4ecdb23c79a7bb09d47
adamfowleruk <adamfowleruk@gmail.com>
2013-05-12 17:59:15 +0100

    Fixed doc generation code

--- gendocs.sh
+++ gendocs.sh
@@ -4,6 +4,9 @@ JSDOC=/Users/adamfowler/Documents/marklogic/git/jsdoc/jsdoc
 
 P=../mldb-pages/apidocs
 D=./dist/docs
+rm -rf $D
+rm -rf $P
+mkdir $P
 mkdir $D
 mkdir $D/core
 mkdir $D/widgets
@@ -15,12 +18,19 @@ echo "Generating widget documentation..."
 $JSDOC -u ./tutorials -c jsdoc-conf.json -d $D/widgets ./mldbwebtest/src/public/js/mldbtest/mldb-*.js ./mldbwebtest/src/public/js/mldbtest/widget*.js browser.md
 echo "...done."
 
-cp -R $D/core $P/core
-cp -R $D/widgets $P/widgets
+cp -R $D/core $P/
+cp -R $D/widgets $P/
 
 #tar czf jsdoc-core.tar.gz $D/core
 #tar czf jsdoc-widgets.tar.gz $D/widgets
 #tar czf jsdoc-all.tar.gz $D
+
+echo "Pushing docs to GitHub Pages"
+cd ../mldb-pages
+git add apidocs
+git commit -a -m "link fix"
+git push origin gh-pages
+cd ../mldb
 
 echo "Done All."
 exit 0
