04bf68253595fd48a7740844f35892255f45f675
adamfowleruk <adamfowleruk@gmail.com>
2013-12-17 15:47:00 +0000

    build updates

--- dist/mljs/nodejs-minified/README.md
+++
@@ -1,9 +0,0 @@
-# MLJS Core developer documentation
-
- - [MLJS Core API documentation](../core/index.html)
- - [MLJS Browser widgets documentation](../widgets/index.html)
- 
-## Tutorials
-
-Be sure to check out the [tutorials](tutorial-all.html).
-
--- dist/mljs/nodejs-minified/lib/basic-wrapper.js
+++
@@ -1,4 +0,0 @@
-
-var http=require('http'),noop=require("./noop");var BasicWrapper=function(){this.configure();};BasicWrapper.prototype.configure=function(username,password,logger){this.username=username;this.password=password;this.logger=logger;};BasicWrapper.prototype.request=function(options,callback_opt){if(undefined!=options.contentType){options.headers["Content-type"]=options.contentType;}
-options.headers["Authorization"]="Basic "+new Buffer(this.username+':'+this.password).toString('base64');return http.request(options,(callback_opt||noop));};BasicWrapper.prototype.get=function(options,callback_opt){if(undefined!=options.contentType){options.headers["Content-type"]=options.contentType;}
-options.headers["Authorization"]="Basic "+new Buffer(this.username+':'+this.password).toString('base64');return http.get(options,(callback_opt||noop));};module.exports=function(){return new BasicWrapper()};
