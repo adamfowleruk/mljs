eb04aa807618d9462ca93d53a25a413ac2744a48
adamfowleruk <adamfowleruk@gmail.com>
2014-02-11 22:46:45 -0500

    params fix

--- run-mljs-webserver.js
+++ run-mljs-webserver.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 var servers = require("mljs-webserver");
 
-var server = new servers.MLJSServer(8080,8081,"http://localhost:8040");
+var server = new servers.MLJSServer(8080,8081,"http://localhost:8040","./mljsrest/src");
 
 // this will now run until we quit
