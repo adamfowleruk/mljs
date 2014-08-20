e41450eb8ecfc55bafaba9135ad4d6fe8bac0eb6
adamfowleruk <adamfowleruk@gmail.com>
2014-08-20 16:02:30 +0100

    Fixed add search options mljs web server script

--- webserver/add-search-options.sh
+++ webserver/add-search-options.sh
@@ -3,9 +3,10 @@
 . webserver-settings.sh
 
 URL="http://$RESTHOST:$RESTPORT/v1/config/query/$1?format=$3"
+USERPASS="$MLADMINUSER:$MLADMINPASS"
 
 echo "Creating search options called $1 of format $3 with file name $2"
-curl -v -X PUT \
+curl -v --anyauth --user $USERPASS -X PUT \
     -d@"$2" \
     -H "Content-type: application/json" \
     $URL
