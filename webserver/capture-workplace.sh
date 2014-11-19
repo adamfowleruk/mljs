3397c2b2b850d1e639a82ffd3fcaee5c4c72239b
adamfowleruk <adamfowleruk@gmail.com>
2014-11-19 10:44:09 +0000

    capture and install workplace fix
    creates directory data if it doesn’t exist
    added messages
    corrected settings sh invocation for linux

--- webserver/capture-workplace.sh
+++ webserver/capture-workplace.sh
@@ -2,7 +2,12 @@
 
 # Config params here
 
-. webserver-settings.sh
+. ./webserver-settings.sh
+
+mkdir -p data
 
 curl -X GET --anyauth --user $MLADMINUSER:$MLADMINPASS  \
     "http://$RESTHOST:$RESTPORT/v1/resources/workplace" > data/mljs-workplace.xml
+
+echo "Capture complete. Output in ./data/mljs-workplace.xml"
+exit 0
--- webserver/install-workplace.sh
+++ webserver/install-workplace.sh
@@ -6,3 +6,6 @@
 
 curl -X POST --anyauth --user $MLADMINUSER:$MLADMINPASS -H "Content-type: text/xml" -d@"./data/mljs-workplace.xml" \
     "http://$RESTHOST:$RESTPORT/v1/resources/workplace"
+
+echo "Completed installing ./data/mljs-workplace.xml in the server's content database"
+exit 0
